b8f3b4b2bc3e254de0f64f452d3607023a749d4d
elaniacs <carensousaesilva@gmail.com>
2022-08-25 18:18:48 -0300

    nextStory and updateUI

--- destini/destini/Controller/ViewController.swift
+++ destini/destini/Controller/ViewController.swift
@@ -13,11 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateUI()
     }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
 
-
+        updateUI()
+    }
+    
+    func updateUI() {
+        if storyBrain.storyNumber > storyBrain.store.count - 1 {
+            print("The end")
+            storyBrain.storyNumber = 0
+        }
+        
+        storyLabel.text = storyBrain.getTitleStory()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
--- destini/destini/Model/StoryBrain.swift
+++ destini/destini/Model/StoryBrain.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct StoryBrain {
+    
+    var store = [
+        Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+    ]
+    
+    var storyNumber = 0
+    
+    mutating func nextStory(userChoice: String) {
+        if userChoice == store[storyNumber].choice1 {
+            storyNumber = storyNumber + 1
+        }
+        else {
+            storyNumber = storyNumber + 2
+        }
+    }
+    
+    func getTitleStory() -> String {
+       return store[storyNumber].title
+    }
+    
+    func getChoice1() -> String {
+        return store[storyNumber].choice1
+    }
+    
+    func getChoice2() -> String {
+        return store[storyNumber].choice2
+    }
+    
+}
